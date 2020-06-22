57a40c4aa05fdc2e4f899d6c35cd0519ce5e2ccd
SpaceWarf <space.warf@gmail.com>
2020-06-22 09:43:37 -0400

    Remove function ban on mintremix

--- commands/user/storytime.js
+++ commands/user/storytime.js
@@ -26,43 +26,21 @@ module.exports = class Storytime extends Command {
         this.loopCntr = 1;
     }
 
-    async addRepeatOption(frames, message) {
-        const filter = reaction => {
-            return repeatEmoji === reaction.emoji.name;
-        };
-
-        await message.react(repeatEmoji);
-        message.awaitReactions(filter, { max: 2, time: 60000, errors: ['time'] })
-            .then(() => {
-                const repeatReaction = message.reactions.get(repeatEmoji);
-                for (const user of repeatReaction.users.values()) {
-                    repeatReaction.remove(user);
-                }
-                this.renderFrames(frames, message);
-            })
-            .catch(() => {
-                message.reactions.get(repeatEmoji).remove();
-            });
-    }
-
     renderFrames(frames, message) {
         setTimeout(() => {
             message.edit(frames[this.loopCntr]);
             this.loopCntr++;
             if (this.loopCntr < frames.length) {
                 this.renderFrames(frames, message);
-            } else {
-                this.loopCntr = 0;
-                // this.addRepeatOption(frames, message);
             }
         }, 1500);
     }
 
     async run(message, { storyName }) {
-        if (message.author.id === "217824315381514241") {
-            await message.say("Not allowed. :rat:");
-            return;
-        }
+//         if (message.author.id === "217824315381514241") {
+//             await message.say("Not allowed. :rat:");
+//             return;
+//         }
         const story = stories[storyName];
         if (!story) {
             await message.say("No story with that name. Use **!list** to see all the available stories.");
