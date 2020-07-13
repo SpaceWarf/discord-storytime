269dc087866027f70498cfa0df96b5a3ad335493
SpaceWarf <space.warf@gmail.com>
2020-07-13 15:38:30 -0400

    Update Twitch polling interval
    polling interval is now 1 minute instead of 1m30s

--- config/twitch.config.js
+++ config/twitch.config.js
@@ -11,13 +11,13 @@ const usernames = [
     "phant0mpup"
 ];
 
-const pollingInterval = 90000;
+const pollingInterval = 60000;
 
 const alertChannel = "bot-channel-🤖";
 
 const customAlerts = {
     "space_warf": "@everyone The rat bastard himself has just gone live!\nhttp://www.twitch.tv/%u% - %t%",
-    "default": "@everyone %u% just went live at http://www.twitch.tv/%u% - %t%"
+    "default": "@everyone %u% just went live\nhttp://www.twitch.tv/%u% - %t%"
 };
 
 module.exports = {
