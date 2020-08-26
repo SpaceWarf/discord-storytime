4dabd72274f67b783f825203ae293ef0642b542a
Gabriel <gabriel@decksign.com>
2020-08-25 20:15:22 -0400

    Fix User Schema

--- db/models/user.js
+++ db/models/user.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 var userSchema = new mongoose.Schema({
     username: String,
-    id: String
+    id: String,
+    nitro: Boolean
 });
 
 module.exports = mongoose.model('users', userSchema);
