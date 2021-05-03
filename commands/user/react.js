1d2ef9e07ed9161bbd6f650ba0285e93207d24ad
SpaceWarf <space.warf@gmail.com>
2021-05-03 10:53:15 -0400

    Make react command accessible to all

--- commands/user/react.js
+++ commands/user/react.js
@@ -25,15 +25,13 @@ module.exports = class List extends Command {
     }
 
     async run(message, { text, id }) {
-        if (message.author.id === this.client.discordUsers.space) {
-            const msgToReact = await message.channel.messages.fetch(id);
-            const emojis = this.getEmojisFromText(text);
-            await message.delete();
+        const msgToReact = await message.channel.messages.fetch(id);
+        const emojis = this.getEmojisFromText(text);
+        await message.delete();
 
-            emojis.forEach(async emoji => {
-                await msgToReact.react(emoji);
-            });
-        }
+        emojis.forEach(async emoji => {
+            await msgToReact.react(emoji);
+        });
     }
 
     getEmojisFromText(text) {
